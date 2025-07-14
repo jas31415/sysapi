c7197f2d9d80d9795fc6a1bfe0de174c93f868a1
jas31415 <yourdankestmeme@gmail.com>
2025-07-14 02:49:37 +0200

    added code to check for OS

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,16 @@
 #include "sysapi.h"
+#include <iostream>
 
 int main()
 {
+#ifdef __linux__
+    std::cout << "linux " << __linux__ << '\n';
+#endif
+#ifdef __WIN32
+    std::cout << "windows32 " << __WIN32 << '\n';
+#endif
+#ifdef __WIN64
+    std::cout << "windows64 " << __WIN64 << '\n';
+#endif
     return 0;
 }
